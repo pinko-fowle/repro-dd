f967f78284edf4d415bac023f83f21c8369a1791
pinko-fowle <mfowle@socialtables.com>
2023-11-07 16:54:00 -0500

    create a second 'logSomething' handler

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
+function logSomething(req, res, next) {
+  console.log({something: 42})
+  return next();
+}
+
 function echoName(req, res, next) {
   res.send(req.params);
   return next();
 }
 
-server.get('/echo/:name', echoName);
+server.get('/echo/:name', logSomething, echoName);
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
