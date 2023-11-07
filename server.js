736db20bea020b1e26dd756c816e5cff43c9d835
pinko-fowle <mfowle@socialtables.com>
2023-11-07 16:48:36 -0500

    launchdarkly: use 'variation' to find key to echo with

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ trace.init();
 
 var restify = require('restify');
 var launchdarkly = require('@launchdarkly/node-server-sdk');
-launchdarkly.init(process.env.LD_SDK_KEY)
+var ldClient = launchdarkly.init(process.env.LD_SDK_KEY)
 
 const server = restify.createServer({
   name: 'repro-dd',
@@ -14,12 +14,23 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-function echoName(req, res, next) {
-  res.send(req.params);
-  return next();
+const LD_CONTEXT = {
+  kind: "user",
+  id: "nobody",
 }
 
-server.get('/echo/:name', echoName);
+async function pickKey(req, res, next) {
+  req.key = await ldClient.variation('echo-key', LD_CONTEXT, req.query.key);
+}
+
+async function echoName(req, res, next) {
+  const value = {
+    [req.key || "name"]: req.params.name
+  };
+  res.send(value);
+}
+
+server.get('/echo/:name', pickKey, echoName);
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
