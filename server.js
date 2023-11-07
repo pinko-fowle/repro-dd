d6e7c2a0cfb2d2d751dd92dabf107a5dc1672def
pinko-fowle <mfowle@socialtables.com>
2023-11-07 16:57:05 -0500

    restify: add /async/:name route & handlers

--- server.js
+++ server.js
@@ -16,13 +16,19 @@ function logSomething(req, res, next) {
   console.log({something: 42})
   return next();
 }
-
 function echoName(req, res, next) {
   res.send(req.params);
   return next();
 }
-
 server.get('/echo/:name', logSomething, echoName);
+
+async function logSomethingAsync(req, res) {
+  console.log({something: "someday"})
+}
+async function echoNameAsync(req, res) {
+  res.send(req.params);
+}
+server.get('/async/:name', logSomethingAsync, echoNameAsync);
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
